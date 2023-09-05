853a3048554bcafada7619f7310f352feabc1339
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-09-05 22:31:34 +0700

    Feat: Add secure route

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 
 const About = () => {
+  const router = useRouter();
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    if (!isLogin) {
+      router.push('/auth/login');
+    }
+  }, []);
+
   return <div>About</div>;
 };
 
