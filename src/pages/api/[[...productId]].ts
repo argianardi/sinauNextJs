2342d3c4d7de1aecc0db0a6e969a28db1c3c7b1b
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-11-04 11:31:50 +0700

    Feat: Add api routes for get product by id

---
+++ src/pages/api/[[...productId]].ts
@@ -0,0 +1,30 @@
+import { retrieveData, retrieveDataById } from '@/utils/db/service';
+
+import { query } from 'firebase/firestore';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// Definisikan tipe data yang akan digunakan untuk respon API (untuk typescript)
+type Data = {
+  status: boolean;
+  statusCode: number;
+  data: any;
+};
+
+// Export sebuah fungsi bernama handler yang akan menangani API request
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log(req?.query?.productId![1]);
+  console.log(req?.query?.productId);
+
+  if (req?.query?.productId![1]) {
+    const data = await retrieveDataById('products', req.query.productId![1]);
+    res.status(200).json({ status: true, statusCode: 200, data });
+  } else {
+    // Data produk yang akan dikirim sebagai respon API
+    const data = await retrieveData('products');
+    // Mengirim respon JSON dengan status 200 (OK) dan data produk
+    res.status(200).json({ status: true, statusCode: 200, data });
+  }
+}
