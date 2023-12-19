ec9f99c47f928f21a6d78b5988b8ba3327300c4e
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-12-19 16:35:15 +0700

    Feat: Add api route with data on local

---
+++ src/pages/api/products-from-local.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// Define tipe data untuk respon API (khusus untuk typescript)
+type Data = {
+  status: boolean;
+  statusCode: number;
+  data: {
+    id: number;
+    name: string;
+    price: number;
+    size: string;
+  }[];
+};
+
+// Export sebuah fungsi bernama handler yang akan menangani API request
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // Data produk yang akan dikirim sebagai respon API
+  const data = [
+    {
+      id: 101,
+      name: 'Sepatu Olahraga Nike Air Zoom Pegasus 38',
+      price: 120.0,
+      size: '42',
+    },
+    {
+      id: 202,
+      name: 'Kamera DSLR Canon EOS 200D',
+      price: 249.0,
+      size: 'N/A',
+    },
+  ];
+
+  // Mengirim respon JSON dengan status 200 (OK) dan data produk
+  res.status(200).json({ status: true, statusCode: 200, data });
+}
